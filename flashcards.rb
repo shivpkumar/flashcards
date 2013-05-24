f121308e1b57041414f2a01850bc23b25d83a684
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-24 12:02:41 -0700

    created flashcard class

--- flashcards.rb
+++ flashcards.rb
@@ -1,1 +1,19 @@
-# Main file for 
+class FlashCard
+
+  attr_reader :question, :marked
+
+  def initialize(args)
+    @question = args[:question]
+    @answer = args[:answer]
+    @marked = args[:marked] || false
+  end
+
+  def guess_correct?(guess)
+    @answer == guess
+  end
+
+  def mark_card!
+    @marked = true
+  end
+
+end
