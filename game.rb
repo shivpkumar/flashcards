cfa2a9c655d2882f21f4e718da1791ad67e17c01
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-24 16:09:37 -0700

    Made Game controller

--- game.rb
+++ game.rb
@@ -1,40 +1,80 @@
-#require_relative "decks.rb"
+require_relative "deck.rb"
 
 class Game
 
-
-  def end_game
+  def initialize(deck)
+    @deck = deck
   end
 
+  def end_game
+    exit
+  end
 
-  deck = Deck.new
+  def start
+    greeting
+    while deck.cards_left?
+      play_round
+    end
+    end_game
+  end
+
+  def play_round
+    @card = deck.get_card
+    show(@card)
+    until next_card?
+      user_input = get_user_response
+      next_card? = respond_to(user_input)
+    end
+  end
+
+  def respond_to(user_input)
+    if user_input == "[next]"
+      return true
+    elsif user_input == "[quit]"
+      end_game
+    elsif user_input == "[help]"
+      help_text
+      return false
+    elsif @card.guess_correct?(user_input)  
+      announce_correct
+      return true
+    else
+      announce_incorrect
+      return false
+    end
+  end
   
-  puts "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition.  Ready?  Go!"
+  def announce_correct
+    puts "Correct!"
+  end
 
-  while
-    card = deck.get_card
+  def announce_incorrect
+    puts "Incorrect!  Try again."
+  end
+
+  def get_user_response
+    gets.chomp!
+  end
+
+  def show(card)
     puts "Definition:"
     puts card.description
-    until correct?
+  end
 
-      print "Guess: "
-      response = gets.chomp!
-      
-      correct? == card.guess_correct
+  def help_text
+    puts "List of commands:"
+    puts " [next] selects a new card"
+    puts " [help] brings up this menu"
+    puts " [quit] exits the program"
+  end
 
-      if response == "[quit]"
-        endgame
-      elsif response == "[next]"
-        next
-      elsif response == "[help]"
-        #print the special commands
-      elsif not correct?
-        puts "Incorrect!  Try again."
-      end
-    end
-    puts "Correct!"
+  def greeting
+    puts "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition.  Ready?  Go!"
   end
 
 end
 
-Game.new
+
+
+game = Game.new(Deck.new('flashcard_samples.txt'))
+game.start
